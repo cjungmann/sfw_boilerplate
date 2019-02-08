6a34d0263604f400ac82eb2a51254bb02bf7d19b
Chuck Jungmann <chuckj60@gmail.com>
2019-02-08 17:29:41 -0600

    Added session tables to illustrate new setup_params variable SITE_SESSION_TABLE.

--- protected/tables.sql
+++ protected/tables.sql
@@ -13,3 +13,21 @@ CREATE TABLE IF NOT EXISTS Person
    gender ENUM('female','male', 'other')
 );
 
+CREATE TABLE IF NOT EXISTS Salt
+(
+   id_user INT UNSIGNED NOT NULL PRIMARY KEY,
+   salt    CHAR(32)
+);
+
+-- Session Information Section (one table)
+CREATE TABLE IF NOT EXISTS Session_Info
+(
+   id_session INT UNSIGNED UNIQUE KEY,
+
+   person_id   INT UNSIGNED,
+   person_name VARCHAR(128),
+
+   INDEX(id_session)
+);
+
+
