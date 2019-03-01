27abb3a41c1501cec086b4113f5686d1b271f8f8
Chuck Jungmann <chuckj60@gmail.com>
2019-03-01 05:31:16 -0600

    Added User table and password recovery tables.

--- protected/tables.sql
+++ protected/tables.sql
@@ -1,33 +1,54 @@
-CREATE TABLE IF NOT EXISTS Item
-(
-   id     INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   name   VARCHAR(40)
-);
+SET default_storage_engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS Person
-(
-   id     INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   fname  VARCHAR(20),
-   lname  VARCHAR(30),
-   dob    DATE,
-   gender ENUM('female','male', 'other')
-);
-
+-- ----------------------------
 CREATE TABLE IF NOT EXISTS Salt
 (
    id_user INT UNSIGNED NOT NULL PRIMARY KEY,
    salt    CHAR(32)
 );
 
--- Session Information Section (one table)
+-- --------------------------------------------
+CREATE TABLE IF NOT EXISTS Password_Reset_Codes
+(
+   id_user            INT UNSIGNED,
+   code               CHAR(6),
+   expires            DATETIME,
+   salt               CHAR(32),
+   attempts_remaining INT UNSIGNED DEFAULT 3,
+
+   INDEX(id_user)
+);
+
+-- ------------------------------------
 CREATE TABLE IF NOT EXISTS Session_Info
 (
    id_session INT UNSIGNED UNIQUE KEY,
 
-   person_id   INT UNSIGNED,
-   person_name VARCHAR(128),
+   -- Application-specific session values:
+   id_user    INT UNSIGNED,
+   id_account INT UNSIGNED,
+   handle     VARCHAR(128),
 
    INDEX(id_session)
 );
 
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS User
+(
+   id       INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   pw_hash  BINARY(16),
+   handle   VARCHAR(128) UNIQUE,
+   email    VARCHAR(128) UNIQUE,
+
+   INDEX(handle),
+   INDEX(email)
+);
+
+-- -------------------------------
+CREATE TABLE IF NOT EXISTS Account
+(
+   id     INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   handle VARCHAR(20) UNIQUE,
+   name   VARCHAR(80)
+);
 
